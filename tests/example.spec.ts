150ae280a8fc054d24ca9ca1907420dcfb5b6208
h kumar <hitesh.ghai@hotmail.com>
2024-11-17 12:26:49 +1300

    changes from second branch

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,6 +6,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
   console.log("Second push");
+  console.log("Changes from second branch");
 });
 
 test('get started link', async ({ page }) => {
